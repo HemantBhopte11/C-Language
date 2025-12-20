48ae4bae3d913ecbb35577ddbd868329edda2b5b
Hemant Bhopte <hemantbhopte2006@gmail.com>
2025-12-20 09:29:32 +0530

    salary of employees

---
+++ Cinstructions/try.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() //Program for difference of two numbers
+{
+                         int a;
+                         int b;
+                         printf("Enter two numbers:");
+                         scanf("%d %d",&a,&b);
+                         int Sum = a-b;
+                         printf("Difference of two numbers is:%d",Sum);
+                         return 0;
+}
