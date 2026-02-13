e6edd4f65949fa9ac8aa84088ffb3c32f1667186
Hemant Bhopte <hemantbhopte2006@gmail.com>
2026-02-13 09:47:02 +0530

    Update Untitled-1.c

--- Basicproblems/Untitled-1.c
+++ Basicproblems/Untitled-1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-int main()
-{
+int main(){
                          printf("Hemant Bhopte\n");
                          printf("Acropolis Institute of Technology and and Research\n");
                          printf("Information Technolgy\n");
